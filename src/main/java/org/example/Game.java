27e0e1c21d5a34de456cbf5a7149b611ed6cf8c7
chang <chang.jj@naver.com>
2025-06-04 13:57:05 +0900

    refactoring-42

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -6,21 +6,34 @@ import java.util.Set;
 public class Game {
 
     public void guess(String guessNumber) {
+        checkForNull(guessNumber);
+        checkForThreeLength(guessNumber);
+        checkForDigit(guessNumber);
+        checkForDuplicate(guessNumber);
+    }
+
+    private void checkForNull(String guessNumber) {
         if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
+    }
 
+    private void checkForThreeLength(String guessNumber) {
         if (guessNumber.length() != 3) {
             throw new IllegalArgumentException();
         }
+    }
 
+    private void checkForDigit(String guessNumber) {
         for (int i = 0; i < guessNumber.length(); i++) {
             char ch = guessNumber.charAt(i);
             if (ch < '0' || ch > '9') {
                 throw new IllegalArgumentException("숫자가 아닌 문자가 포함되어 있습니다: " + ch);
             }
         }
+    }
 
+    private void checkForDuplicate(String guessNumber) {
         Set<Character> seen = new HashSet<>();
         for (int i = 0; i < guessNumber.length(); i++) {
             char ch = guessNumber.charAt(i);
@@ -32,7 +45,5 @@ public class Game {
 
             seen.add(ch);
         }
-
-
     }
 }
